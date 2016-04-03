f158fec9b5740ba43006e61752eadd8f9afcd86c
pagelab.io <pagelab.io@gmail.com>
2016-04-02 23:21:22 -0400

    Pagelab first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html itemscope itemtype="http://schema.org/Thing" lang="es-MX">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PageLab</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="desarrollo, web, html5, css3, javascript, laravel, php, mysql">
+        <meta name="author" content="PageLab" />
+        <meta name="robots" content="INDEX, FOLLOW, ARCHIVE">
+
+        <!-- Favicons -->
+        <!-- <link rel="icon" href="url" type="image/x-icon" /> -->
+        <!-- <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png" /> -->
+        <!-- <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png" /> -->
+
+        <!-- Schema properties -->
+        <meta itemprop="name" content="PageLab">
+        <meta itemprop="description" content="page-description">
+        <meta itemprop="url" content="http://pagelab.io">
+        <meta itemprop="image" content="image-url">
+
+        <!-- Open Graph properties -->
+        <meta property="fb:app_id" content="app-id" />
+        <meta property="og:site_name" content="PageLab">
+        <meta property="og:title" content="PageLab"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="http://pagelab.io"/>
+        <meta property="og:image" content="image-url"/>
+        <meta property="og:description" content="page-description" />
+
+        <!-- Twitter integration -->
+        <meta name="twitter:title" content="PageLab" />
+        <meta name="twitter:image" content="image-url" />
+        <meta name="twitter:url" content="http://pagelab.io" />
+        <meta name="twitter:card" content="" />
+
+        <!-- Page styles -->
+        <link href="css/styles.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <div class="cover">
+			<div class="logo-pagelab"></div>
+        </div>
+
+        <!-- Google Analytics -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-70493809-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    </body>
+</html>
