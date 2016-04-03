9700f271351390ca6832ba4b4f00a72f7da4d9c0
pagelab.io <pagelab.io@gmail.com>
2016-04-03 00:55:36 -0400

    add git-request file in roder to update automatically the project

---
+++ public/git-request.php
@@ -0,0 +1,17 @@
+<?php
+
+// Init vars
+$REPO_PATH = "/var/www/pagelab.io";
+
+//echo "<pre>";
+
+//echo shell_exec("cd ~/ && pwd 2>&1");
+
+// Pull files from repo
+$output = shell_exec("cd /var/www/pagelab.io &&  git pull 2>&1");
+
+// Log changes
+file_put_contents("/var/www/logs/pagelab.log", '/////////////////////////////' . PHP_EOL . $output, FILE_APPEND);
+
+// END
+die("<br>$output </pre>" . mktime());
